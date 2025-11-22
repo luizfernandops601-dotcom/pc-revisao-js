8c3950d46710ea0684b0404fa01792d57a1e54c5
luizfernandop.s601@gmail.com <luizfernandop.s601@gmail.com>
2025-11-22 11:24:34 -0300

    feat: creat

---
+++ 01.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+let chocpoGlobal = 10// escoopo global
+
+function validaDescnto(volorcopra){
+   let desconto = 0 //escopo de funcao
+ 
+    if(volorcopra >= 100){
+        desconto = volorcopra * 0.10
+    }
+       
+    valorFinal = volorcopra - desconto
+
+    return{
+        valorCompra,
+        desconto,
+        valorfinal,
+    }
+
+
+}
+
+console.log(validaDescnto(1200));
+
+
+
+
+
+
+
+
